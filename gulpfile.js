50500b8f004bb6e32fd603cf490b17e855f4f681
phildan02 <phildan02@gmail.com>
2022-10-12 20:25:59 +0300

    added video

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'app/js/jquery.fancybox.min.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
